85ac4771cfd0e555708b3a0a246151a9ceecd47d
DESKTOP-E7L2H89\Kroniks <wowmandarin@yandex.ru>
2026-03-27 00:10:32 +0300

    Updated

--- Source/LifeExeTestProjectEditor/LifeExeTestProjectEditor.cpp
+++ Source/LifeExeTestProjectEditor/LifeExeTestProjectEditor.cpp
@@ -1,4 +1,34 @@
 #include "LifeExeTestProjectEditor.h"
-#include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, LifeExeTestProjectEditor, "LifeExeTestProjectEditor");
+#include "Modules/ModuleManager.h"
+#include "PropertyEditorModule.h"
+
+#define LOCTEXT_NAMESPACE "FLifeExeTestProjectEditorModule"
+
+IMPLEMENT_MODULE(FLifeExeTestProjectEditorModule, LifeExeTestProjectEditor)
+
+void FLifeExeTestProjectEditorModule::StartupModule()
+{
+    FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+
+    TSharedRef<FPropertySection> Section = PropertyModule.FindOrCreateSection(
+        "TestMovementActor",
+        "ConfigData",
+        LOCTEXT("ConfigData", "ConfigData"));
+
+    Section->AddCategory("ConfigData");
+    PropertyModule.NotifyCustomizationModuleChanged();
+}
+
+void FLifeExeTestProjectEditorModule::ShutdownModule()
+{
+    if(FModuleManager::Get().IsModuleLoaded("PropertyEditor"))
+    {
+        FPropertyEditorModule& PropertyModule = FModuleManager::GetModuleChecked<FPropertyEditorModule>("PropertyEditor");
+
+        PropertyModule.RemoveSection("TestMovementActor", "ConfigData");
+        PropertyModule.NotifyCustomizationModuleChanged();
+    }
+}
+
+#undef LOCTEXT_NAMESPACE
